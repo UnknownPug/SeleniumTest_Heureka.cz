e35025ac0caf0d96e0b20953e9e469822cf103de
Krul, Filip <krulfili@fel.cvut.cz>
2022-05-15 22:42:23 +0200

    enabled acceptCookies in the big test

--- heureka-scraper/src/test/java/cz/cvut/ts1/seleniumheureka/AppTest.java
+++ heureka-scraper/src/test/java/cz/cvut/ts1/seleniumheureka/AppTest.java
@@ -4,28 +4,28 @@ import org.junit.jupiter.api.Test;
 
 public class AppTest extends TestCase {
 
-    @Test
-    public void shouldAnswerWithTrue() {
-        new MainPage(getDriver())
-                //.acceptCookies()
-                .goToLoginPage()
-                .login("login", "password") // Enter your login and password before start testing
-                .navToLaptopsPage()
-                .laptopInfo()
-                .laptopPriceDescriptionOne()
-                .backToLaptopPage()
-                .laptopPriceDescriptionTwo()
-                .backToLaptopPage()
-                .openThirdLaptopDescription()
-                .addThirdLaptopToUserBasket()
-                .goToMainPage()
-                .navToCarPage()
-                .skodaAutoDetails()
-                .advisoryCarSearch()
-                .navToHowToChooseAuto()
-                .goBackToMainPage()
-                .navToHeurekaProfile()
-                .logOut();
-        System.out.println(); //Select this line and run debug to see the testing process
-    }
+  @Test
+  public void shouldAnswerWithTrue() {
+    new MainPage(getDriver())
+      .acceptCookies()
+      .goToLoginPage()
+      .login("login", "password") // Enter your login and password before start testing
+      .navToLaptopsPage()
+      .laptopInfo()
+      .laptopPriceDescriptionOne()
+      .backToLaptopPage()
+      .laptopPriceDescriptionTwo()
+      .backToLaptopPage()
+      .openThirdLaptopDescription()
+      .addThirdLaptopToUserBasket()
+      .goToMainPage()
+      .navToCarPage()
+      .skodaAutoDetails()
+      .advisoryCarSearch()
+      .navToHowToChooseAuto()
+      .goBackToMainPage()
+      .navToHeurekaProfile()
+      .logOut();
+    System.out.println(); //Select this line and run debug to see the testing process
+  }
 }
